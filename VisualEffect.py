7f88e8a6611a566ed6498f4dfd28d53168327f2b
Bruno Ferreira <97362671+Brun0MF@users.noreply.github.com>
2023-02-28 14:25:15 +0000

    Main Code

---
+++ VisualEffect.py
@@ -0,0 +1,21 @@
+#Visual Effect: Console Color
+def red(txt): return "\033[91m"+txt+"\033[00m"
+
+def green(txt): return "\033[92m"+txt+"\033[00m"
+
+def yellow(txt): return "\033[93m"+txt+"\033[00m"
+
+def purple(txt): return"\033[95m"+txt+"\033[00m"
+ 
+def cyan(txt): return"\033[96m"+txt+"\033[00m"
+ 
+def orange(txt): return "\033[33m"+txt+"\033[00m"
+
+def blue(txt): return"\033[34m"+txt+"\033[00m"
+ 
+def pink(txt): return"\033[95m"+txt+"\033[00m"
+
+#Visual Effect: Console Style
+def bold(txt): return"\033[01m"+txt+"\033[00m"
+
+def underlined(txt): return"\033[04m"+txt+"\033[00m"
